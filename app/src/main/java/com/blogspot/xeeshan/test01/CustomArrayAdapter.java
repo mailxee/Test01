1e973df9009404c22fe8b7f1f78f1a01ad13f2d2
mailxee <mailxee@gmail.com>
2015-07-04 10:50:14 +0500

    FromLaptop

---
+++ app/src/main/java/com/blogspot/xeeshan/test01/CustomArrayAdapter.java
@@ -0,0 +1,49 @@
+package com.blogspot.xeeshan.test01;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Zeeshan on 21/06/2015.
+ */
+public class CustomArrayAdapter<T> extends ArrayAdapter<T>
+{
+    private TextView tempTextView;
+    private int counter;
+    public CustomArrayAdapter(Context context,int layoutResource,int textViewResource)
+    {
+        super(context,layoutResource,textViewResource);
+        counter=0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View tempView = super.getView(position, convertView, parent);
+        if (tempView.getTag()==null)
+        {
+            tempTextView = (TextView) tempView.findViewById(R.id.textView_01);
+            //can set tag to the textview itself as well! don't really need a view holder!!
+            tempView.setTag(new MyViewHolder(tempTextView));
+            counter++;
+        }
+        else
+        {
+            tempTextView = ((MyViewHolder) tempView.getTag()).textView_01;
+        }
+        tempTextView.setText("crikey "+counter);
+        return tempView;
+
+    }
+    public class MyViewHolder
+    {
+        public TextView textView_01;
+        public MyViewHolder(TextView textView_01)
+        {
+            this.textView_01=textView_01;
+        }
+    }
+}
---
+++ app/src/main/java/com/blogspot/xeeshan/test01/MainActivity.java
@@ -0,0 +1,55 @@
+package com.blogspot.xeeshan.test01;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //find the listView in xml
+        ListView myListView=(ListView)findViewById(R.id.listView);
+        //create a custom array adapter which will store the objects (strings in this case)
+        ArrayAdapter<String> myArrayAdapter=
+                new CustomArrayAdapter<>(this, R.layout.listview_item_complex, R.id.textView);
+        //populate the array adapter
+        for (int i=0;i<100;++i)
+            myArrayAdapter.add("String number " + i);
+        //attach the array adapter to the list view
+        myListView.setAdapter(myArrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
